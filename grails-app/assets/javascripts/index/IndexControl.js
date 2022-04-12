50eebf0269aa4e26b5901e569cf74afed4663a97
Hebert Barbosa <hebert.barbosa@asaas.com.br>
2022-04-11 22:36:57 -0300

    revisando js

--- grails-app/assets/javascripts/index/IndexControl.js
+++ grails-app/assets/javascripts/index/IndexControl.js
@@ -21,129 +21,196 @@ formReference.addEventListener("submit", (event) => {
         customer[key] = value;
     });
     console.log(customer)
-    if(checkInputs(customer)){
+    if(checkInputs()){
         alert("Enviado com sucesso!!");
         formReference.reset();
         }else{  
         alert("Preencha os campos em branco");
     }
+
      
-
-function checkInputs(customer){
-    var validationOk;
-    if(!customer.username.trim()){
-        setInputAsError(usernameInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else{
-        successValidation(usernameInputReference)
-        validationOk = true;
-        return;
-    }
-
-    if(!customer.email.trim()){
-        setInputAsError(emailInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else{
-        successValidation(emailInputReference)
-        validationOk = true;
-    }
-
-    if(!customer.cpfcnpj.trim()){
-        setInputAsError(cpfcnpjInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else if(cpfcnpjInputReference.length > 14){
-        setInputAsError(cpfcnpjInputReference, "Esse campo deve ter no máximo 14 caracteres")
-        validationOk = false;
-    }else{
-        successValidation(cpfcnpjInputReference)
-        validationOk = true;
-    }
-
-    if(!customer.telefone.trim()){
-        setInputAsError(telefoneInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else{
-        successValidation(telefoneInputReference)
-        validationOk = true;
-    }
-
-    if(!customer.cep.trim()){
-        setInputAsError(cepInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else{
-        successValidation(cepInputReference)
-        validationOk = true;
-    }
-
-    if(!customer.endereco.trim()){
-        setInputAsError(enderecoInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else{
-        successValidation(enderecoInputReference)
-        validationOk = true;
-    }
-
-    if(!customer.numero.trim()){
-        setInputAsError(numeroInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else{
-        successValidation(numeroInputReference)
-        validationOk = true;
-    }
-
-    if(!customer.bairro.trim()){
-        setInputAsError(bairroInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else{
-        successValidation(bairroInputReference)
-        validationOk = true;
-    }
-
-    if(!customer.cidade.trim()){
-        setInputAsError(cidadeInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else{
-        successValidation(cidadeInputReference)
-        validationOk = true;
-    }
-
-    if(!customer.estado.trim()){
-        setInputAsError(estadoInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else if(estadoInputReference.length > 2){
-        setInputAsError(estadoInputReference, "Apenas 2 caracteres")
-        validationOk = false;
-    }else{
-        successValidation(estadoInputReference)
-        validationOk = true;
-    }
-
-    if(!customer.password.trim()){
-        setInputAsError(passwordInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else if(passwordInputReference.length < 8){
-        setInputAsError(passwordInputReference, "A senha deve ter no mínimo 8 caracteres")
-        validationOk = false;
-    }else{
-        successValidation(passwordInputReference)
-        validationOk = true;
-    }
-
-    if(!customer.confirmPassword.trim()){
-        setInputAsError(confirmPasswordInputReference, "Preencha esse campo")
-        validationOk = false;
-    }else{
-        successValidation(confirmPasswordInputReference)
-        validationOk = true;
-    }
-
-    if(validationOk){
-        return true;
-    }else{
-        return;
+function checkInputs(){
+    if(
+    checkInputUserName(customer) &&
+    checkInputeMail(customer) &&
+    checkInputCpfCnpj(customer) &&
+    checkInputTelefone(customer) &&
+    checkInputCep(customer) &&
+    checkInputEndereco(customer) &&
+    checkInputNumero(customer) &&
+    checkInputBairro(customer) &&
+    checkInputCidade(customer) &&
+    checkInputEstado(customer) &&
+    checkInputPassword(customer) &&
+    checkInputConfirmPassword(customer)){
+        return true
     }
     
-}
+};
+
+function checkInputUserName(customer){
+    //var validationOk;
+    if(!customer.username.trim()){
+        setInputAsError(usernameInputReference, "Preencha esse campo");
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(usernameInputReference);
+        //validationOk = true;
+        return true;
+        
+    }};
+   
+function checkInputeMail(customer){
+    if(!customer.email.trim()){
+        setInputAsError(emailInputReference, "Preencha esse campo");
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(emailInputReference);
+        //validationOk = true;
+        return true;
+        
+    }};
+    
+function checkInputCpfCnpj(customer){
+    if(!customer.cpfcnpj.trim()){
+        setInputAsError(cpfcnpjInputReference, "Preencha esse campo");
+        //validationOk = false;
+        return false;
+    }else if(cpfcnpjInputReference.length > 14){
+        setInputAsError(cpfcnpjInputReference, "Esse campo deve ter no máximo 14 caracteres");
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(cpfcnpjInputReference);
+        //validationOk = true;
+        return true;
+        
+    }};
+
+function checkInputTelefone(customer){
+    if(!customer.telefone.trim()){
+        setInputAsError(telefoneInputReference, "Preencha esse campo")
+        return false;
+    }else{
+        successValidation(telefoneInputReference)
+        //validationOk = true;
+        return true;
+        
+    }};
+
+function checkInputCep(customer){
+    if(!customer.cep.trim()){
+        setInputAsError(cepInputReference, "Preencha esse campo")
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(cepInputReference)
+        //validationOk = true;
+        return true;
+        
+    }};
+
+function checkInputEndereco(customer){
+    if(!customer.endereco.trim()){
+        setInputAsError(enderecoInputReference, "Preencha esse campo")
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(enderecoInputReference)
+        //validationOk = true;
+        return true;
+        
+    }};
+
+function checkInputNumero(customer){
+    if(!customer.numero.trim()){
+        setInputAsError(numeroInputReference, "Preencha esse campo")
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(numeroInputReference)
+        //validationOk = true;
+        return true;
+        
+    }};
+
+function checkInputBairro(customer){
+    if(!customer.bairro.trim()){
+        setInputAsError(bairroInputReference, "Preencha esse campo")
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(bairroInputReference)
+        //validationOk = true;
+        return true;
+        
+    }};
+
+function checkInputCidade(customer){
+    if(!customer.cidade.trim()){
+        setInputAsError(cidadeInputReference, "Preencha esse campo")
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(cidadeInputReference)
+        //validationOk = true;
+        return true;
+        
+    }};
+
+function checkInputEstado(customer){
+    if(!customer.estado.trim()){
+        setInputAsError(estadoInputReference, "Preencha esse campo")
+        //validationOk = false;
+        return false;
+    }else if(estadoInputReference.length > 2){
+        setInputAsError(estadoInputReference, "Apenas 2 caracteres")
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(estadoInputReference)
+        //validationOk = true;
+        return true;
+        
+    }};
+
+function checkInputPassword(customer){
+    if(!customer.password.trim()){
+        setInputAsError(passwordInputReference, "Preencha esse campo")
+        //validationOk = false;
+        return false;
+    }else if(passwordInputReference.length < 8){
+        setInputAsError(passwordInputReference, "A senha deve ter no mínimo 8 caracteres")
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(passwordInputReference)
+        //validationOk = true;
+        return true;
+        
+    }};
+    
+function checkInputConfirmPassword(customer){
+    if(!customer.confirmPassword.trim()){
+        setInputAsError(confirmPasswordInputReference, "Preencha esse campo")
+        //validationOk = false;
+        return false;
+    }else{
+        successValidation(confirmPasswordInputReference)
+        //validationOk = true;
+        return true;
+    }};
+
+    //if(validationOk){
+    //
+   // }else{
+   //     return;
+   // }
+});
+    
+
 function setInputAsError(input, message){
     const formControl = input.parentElement;
     const errorMessageContainer = formControl.querySelector(".js-error-note");
@@ -161,20 +228,18 @@ function successValidation(input){
 
 
 var preencherFormulario = (endereco) =>{
-    document.getElementById("endereco").value = endereco.logradouro;
-    document.getElementById("bairro").value = endereco.bairro;
-    document.getElementById("cidade").value = endereco.localidade;
-    document.getElementById("estado").value = endereco.uf;
+    enderecoInputReference.value = endereco.logradouro;
+    bairroInputReference.value = endereco.bairro;
+    cidadeInputReference.value = endereco.localidade;
+    estadoInputReference.value = endereco.uf;
   }
   
 var pesquisarCep = async() => {
-    var cep = document.getElementById("cep").value;
+    var cep = cepInputReference.value;
     var url = `http://viacep.com.br/ws/${cep}/json/`;
     var dados = await fetch(url);
     var endereco = await dados.json();
     preencherFormulario(endereco);
   }
   
-  document.getElementById("cep")
-    .addEventListener("focusout", pesquisarCep);
-})
+  cepInputReference.addEventListener("focusout", pesquisarCep);
